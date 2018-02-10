bf0cae982cf32bac8be67530058852f03742d37c
Andy Maskiell <maskiella@gmail.com>
2018-02-10 07:08:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
     erb :operation
